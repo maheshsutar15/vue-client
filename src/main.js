e048674a14a1da34839a41990376a1e6b314dea2
Kaustubh Murumkar <kaustubh.murumkar@gmail.com>
2021-05-25 00:27:55 +0530

    vuex store

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import 'mutationobserver-shim'
 import './plugins/bootstrap-vue'
 import Vue from 'vue'
 import App from './App.vue'
-// import Router from 'vue-router'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  store,
+  render: h => h(App)
 }).$mount('#app')
